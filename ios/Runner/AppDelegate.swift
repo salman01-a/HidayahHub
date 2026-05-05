fc03ef77679ca1f28ecc65409f1d6c55c0c54f4a
Reza Rasendriya <reza58912@gmail.com>
2026-05-05 20:12:46 +0700

    ai

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,10 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool { 
+    GMSServices.provideAPIKey("AIzaSyAEzyLDetxcFgw64xC4VG7lUIvmWJ95GHU")
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
