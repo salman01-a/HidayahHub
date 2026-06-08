342127e113d5e4b4e24a6a7795e760c9d9a18b8c
salman <salmanfaris10072005@gmail.com>
2026-06-09 05:36:42 +0700

    dikit perbaikan

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,13 +7,8 @@ import GoogleMaps
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    
-    GMSServices.provideAPIKey("AIzaSyAEzyLDetxcFgw64xC4VG7lUIvmWJ95GHU")
-    if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
-    }
-    
+  ) -> Bool { 
+   
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
